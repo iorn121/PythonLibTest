98f299f411215585e11d0f25e42a7bd3737e4640
iorn <wmt.tkn.121@gmail.com>
2022-06-15 17:32:07 +0900

    getter setter

--- standard/test.py
+++ standard/test.py
@@ -1,21 +1,36 @@
 def document_it(func):
     def show_info(*args, **kwargs):
-        print('function name: ',func.__name__)
-        print('args: ',args)
-        print('kwargs: ',kwargs)
-        result=func(*args, **kwargs)
-        print('result: ',result)
+        print('function name: ', func.__name__)
+        print('args: ', args)
+        print('kwargs: ', kwargs)
+        result = func(*args, **kwargs)
+        print('result: ', result)
     return show_info
 
 
 class Cat:
-    def __init__(self, name):
-        self.name =name
+    def __init__(self, input_name):
+        self.name = input_name
+
+    @property
+    def name(self):
+        print('inside the getter')
+        return self.name
+
+    @name.setter
+    def name(self, input_name):
+        print('inside the setter')
+        self.name = input_name
 
 
 @document_it
-def sum(a,b):
+def sum(a, b):
     return a+b
 
-cat=Cat('Kuro')
-print(cat.name)
+
+cat = Cat('Kuro')
+# cat.name
+
+# cat.name = 'Shiro'
+
+# cat.name
